3b41003a549e835dae8c4ada0fc768b4a9e1da14
n-zad <bayati@calpoly.edu>
2026-02-28 15:15:51 -0800

    update package-lock.json and Dashboard.js

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,12 +1,18 @@
 import React from "react";
-function Dashboard(){
-    return(
-        <div> 
-            <h1> Dashboard</h1>
-            <div className = 'your-events'/>
-            <p>There are no new upcoming events</p>
-            <div className = 'upcoming-events'/>
-            <p>There are no new upcoming events</p>
-        </div>
-    )
+import { Link } from "react-router-dom";
+
+function Dashboard() {
+  return (
+    <div>
+      <h1> Dashboard</h1>
+      <div className="your-events" />
+      <p>There are no new upcoming events</p>
+      <div className="upcoming-events" />
+      <p>There are no new upcoming events</p>
+
+      <Link to="/test">Calendar test page</Link>
+    </div>
+  );
 }
+
+export default Dashboard;
