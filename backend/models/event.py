189ed159eb1dc7593e10d877f2dea0adc5f5c085
TopJdubbz <mwickler@scu.edu>
2026-02-28 17:55:59 -0800

    updated event class to include interest

--- backend/models/event.py
+++ backend/models/event.py
@@ -1,8 +1,13 @@
 class Event:
-    def __init__(self, name, date, location, time):
+    def __init__(self, interest, name, date, location, time):
+        self.interest = interest
         self.name = name
         #Edit location as we change location
         self.location = location
         self.date = date
         #self.capacity = capacity
         self.time = time
+    
+    @classmethod
+    def createEvent(cls, interest, name, date, location, time):
+        return cls(interest, name, date, location, time)
