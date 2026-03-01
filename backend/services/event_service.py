2f2c2bb66a7060445cdc457263d3bd79d1da31fd
TopJdubbz <mwickler@scu.edu>
2026-02-28 17:55:06 -0800

    included interest in addevent and stuff

--- backend/services/event_service.py
+++ backend/services/event_service.py
@@ -1,7 +1,7 @@
 import sqlite3
 import models.event as Event
 def create_table():
-    cursor.execute('CREATE TABLE IF NOT EXISTS events (name TEXT, location TEXT, category TEXT, time DATETIME)')
+    cursor.execute('CREATE TABLE IF NOT EXISTS events (interest TEXT, name TEXT, location TEXT, category TEXT, time DATETIME)')
 
 
 with sqlite3.connect('events.db') as connection:
@@ -10,19 +10,19 @@ with sqlite3.connect('events.db') as connection:
     create_table()
 
 
-def addEvent(name, location, category, time):
-    newEvent = Event(name, category, location, time)
-    cursor.execute('INSERT INTO events VALUES (?, ?, ?, ?)', (newEvent.name, newEvent.location, newEvent.category, newEvent.time))
+def addEvent(interest, name, location, category, time):
+    newEvent = Event(interest, name, category, location, time)
+    cursor.execute('INSERT INTO events VALUES (?, ?, ?, ?, ?)', (newEvent.info, newEvent.name, newEvent.location, newEvent.category, newEvent.time))
 
 
 def getEvents(category):
     events = []
     for row in cursor.execute('SELECT * FROM events WHERE name = ?', (category,)):
-        events.append(Event(row[0], row[1], row[2], row[3]))
+        events.append(Event(row[0], row[1], row[2], row[3], row[4]))
     return events 
 
 def getTopTenEventsByDate():
     events = []
     for row in cursor.execute('SELECT * FROM events ORDER BY time DESC LIMIT 10'):
-        events.append(Event(row[0], row[1], row[2], row[3]))
+        events.append(Event(row[0], row[1], row[2], row[3], row[4]))
     return events
